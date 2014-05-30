dbf4411d81d10d87b34ab8a39dedd709be78eb8b
Steelknight <as.avramenko@yandex.ru>
2014-05-30 19:00:20 +0400

    9.2.1 $.ajax

---
+++ testAjax.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alexey
+ * Date: 29.05.14
+ * Time: 19:17
+ */
+   header('Content-Type: text/html; charset=utf-8');
+   if ($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
+      for ($i=0; $i<2; $i++) {
+         sleep(1);
+      }
+      if ($_POST) {
+         print '<h3>Ответ сервера</h3><p>Время: ' . date('H:i:s', time()) . '</p>';
+      }
+   }
+?>
